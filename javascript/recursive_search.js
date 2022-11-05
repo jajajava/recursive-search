d879fe21272609bc3b07dbfcb3fdded81bb157a0
David <davidstepanov@yahoo.com>
2022-11-05 19:11:37 -0400

    commit tree c47716b1a5ee2dd19414b85c4de3f69b3cee6135

--- javascript/recursive_search.js
+++ javascript/recursive_search.js
@@ -1,6 +1,20 @@
+let array = [1,2,3,4,5,6,7,8,9,10]
+
 function recursiveSearch(arr, target) {
-  // type your code here
+   let evens = []
+   let odds = []
+  arr.forEach(eachOne => {
+    eachOne % 2 == 0 ? 
+    evens.push(eachOne)
+    : odds.push(eachOne)
+  })
+  if(target %2 == 0){
+    evens.forEach(each => each === target? console.log("Number was found!") : console.log("Number was not found!"))
+  } else if (target %2 != 0){
+      odds.forEach(each => each === target? console.log("Number was found!") : console.log("Number was not found!"))
+    } 
 }
+console.log(recursiveSearch(array, 10))
 
 if (require.main === module) {
   // add your own tests in here
